8419c95e03edf6d98ee1e5fa9b06e88f2be9e7c4
Barry Earsman <barry@earsman.com>
2018-08-24 09:13:31 +1000

    Initial commit

---
+++ contracts/LiquidDecisions.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.0;
+contract LiquidDecisions {
+    
+    uint proposalCount;
+
+    struct Proposal {
+        uint id;
+        string title;
+        string uri;
+        address proposer;
+        uint proposalDate;
+        uint expiryDate;
+        string tag;
+    }
+    
+    struct Delegatee {
+        address addr;
+        string name;
+    }
+    
+    event CastVoteEvent(address voter, uint proposalId, bool value);
+    event DelegateVoteEvent(address voter, uint proposalId, address delegatee);
+    event RegisterDelegateeEvent(address delegatee, string name);
+    event DelegateTaggedVotesEvent(address voter, string tag, address delegatee);
+
+    Proposal[] public proposals;
+    Delegatee[] public delegatees;
+    
+
+    constructor () public {
+        proposalCount = 0;
+    }
+    
+    function makeProposal(string title, string uri, uint duration, string tag) public {
+        proposals.push(Proposal(
+            proposalCount,
+            title,
+            uri,
+            msg.sender,
+            now,
+            now + duration,
+            tag
+        ));
+        proposalCount ++;
+    }
+
+    function castVote(uint proposalId, bool value) public {
+        //Stateless
+        emit CastVoteEvent(msg.sender, proposalId, value);
+    }
+    
+    function delegateVote(uint proposalId, address delegatee) public {
+        //Stateless
+       emit DelegateVoteEvent(msg.sender, proposalId, delegatee);
+    }
+    
+    function delegateTaggedVotes(string tag, address delegatee) public {
+        //Stateless
+        emit DelegateTaggedVotesEvent(msg.sender, tag, delegatee);
+    }
+    
+    function registerDelegatee(string name) public {
+        delegatees.push(Delegatee(msg.sender, name));
+        emit RegisterDelegateeEvent(msg.sender, name);
+    }
+
+}
