d60252cf18bc6fbfee8bd864ed326279cff99517
Barry Earsman <barry@earsman.com>
2018-09-18 17:25:32 +1000

    Fix for provider.sendAsync is not a function

--- app/src/modules/LiquidDecisions.ts
+++ app/src/modules/LiquidDecisions.ts
@@ -1,11 +1,13 @@
-const Web3 = require('web3')
+//const Web3 = require('web3')
+import * as Ethers from 'ethers'
 import { resolve } from 'url';
 var contractJson = require('../../../ethereum/build/contracts/LiquidDecisions.json')
 
 //const web3 = new Web3('https://ropsten.infura.io/s1tfpFETHbLYVlvd7CRk');
 //const web3Reader = new Web3('https://rinkeby.infura.io/s1tfpFETHbLYVlvd7CRk');
-const web3Reader = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"))
 
+//const web3Reader = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"))
+/*
 var web3Writer: any
 if (window && window['web3']) {
     web3Writer = window?window['web3']:web3Reader
@@ -13,13 +15,24 @@ if (window && window['web3']) {
 else {
     web3Writer = web3Reader
 }
+*/
 
 const NETWORK_ID = "5777" //
 var SEC_DAY = 60 * 60 * 24
 var STARTING_BLOCK = 1;
 const abi = contractJson.abi
-const contractReader: any = new web3Reader.eth.Contract(abi, contractJson.networks[NETWORK_ID].address)
-const contractWriter: any = (web3Reader == web3Writer)?contractReader:web3Writer.eth.contract(abi).at(contractJson.networks[NETWORK_ID].address)
+const contractAddress = contractJson.networks[NETWORK_ID].address
+
+const contractReaderProvider = new Ethers.providers.JsonRpcProvider("http://127.0.0.1:8545")//, {chainId: NETWORK_ID})
+const contractReader = new Ethers.Contract(contractAddress, abi, contractReaderProvider)
+
+//window['web3'].providers.HttpProvider.prototype.sendAsync = window['web3'].providers.HttpProvider.prototype.send;
+const contractWriterProvider = new Ethers.providers.Web3Provider(window['web3'].currentProvider)
+
+contractWriterProvider.sendAsync = contractWriterProvider.send
+const contractWriterSigner = contractWriterProvider.getSigner()
+const contractWriter = new Ethers.Contract(contractAddress, abi, contractWriterSigner)
+//web3Writer.eth.contract(abi).at(contractJson.networks[NETWORK_ID].address)
 
 //const contractWriter = uport.contract(abi).at(abi.deployedAddress);
 
@@ -51,7 +64,14 @@ export type Delegatee = {
 export namespace Contract {
     export async function getEvents(proposalId: number)  {
 
-        let events: Array<any> = (await contractReader.getPastEvents(
+        var filter = {
+            fromBlock: STARTING_BLOCK,
+            address: contractAddress
+        }
+        let events: Array<any> = contractReaderProvider.getLogs(filter).filter((item)=>(item.returnValues.proposalId == proposalId))
+       // filter((item)=>(item.returnValues.proposalId == proposalId))
+
+        /*let events: Array<any> = (await contractReader.getPastEvents(
             'allEvents',
             {
                 //filter: {proposalId: proposalId.toString()}, 
@@ -59,21 +79,21 @@ export namespace Contract {
                 toBlock: 'latest'
             },
         )).filter((item)=>(item.returnValues.proposalId == proposalId))
-
+*/
         return events 
     }
 
-    export function makeProposal(title: string, uri: string, duration: number, tag: string): Promise<any> {  
-        return new Promise<any>((resolve, reject) => {
-            contractWriter.makeProposal(title, uri, duration * SEC_DAY, tag, (err: Error, data: any) => {
+    export async function makeProposal(title: string, uri: string, duration: number, tag: string): Promise<any> {  
+        return new Promise<any>(async (resolve, reject) => {
+            return await contractWriter.makeProposal(title, uri, duration * SEC_DAY, tag)/*, (err: Error, data: any) => {
                 if (err) {
                     reject(err)
                 }
                 else {
                     resolve(data)
                 }
-            })
-        })  
+            })*/
+        })
     }
 
     export function registerDelegatee(name: string): Promise<any> {  
@@ -131,11 +151,11 @@ export namespace Contract {
     export async function getDelegatees(): Promise<Delegatee[]> {
         return new Promise<Delegatee[]>(async (resolve, reject) => {
             try {
-                let count = parseInt(await contractReader.methods.delegateeCount().call(), 10);
+                let count = parseInt(await contractReader.delegateeCount(), 10);
                 let delegatees: Delegatee[] = []
         
                 for (let i=0; i< count; i++) {
-                    let delegatee: Delegatee = await contractReader.methods.delegatees(i).call()
+                    let delegatee: Delegatee = await contractReader.delegatees(i)
                     delegatee.key = i;
                     delegatees.push(delegatee);
                 }
@@ -152,11 +172,11 @@ export namespace Contract {
 
         return new Promise<Proposal[]>(async (resolve, reject) => {
             try {
-                let count = parseInt(await contractReader.methods.proposalCount().call(), 10);
+                let count = parseInt(await contractReader.proposalCount(), 10);
                 let proposals: Proposal[] = []
         
                 for (let i=0; i< count; i++) {
-                    let proposal: Proposal = await contractReader.methods.proposals(i).call()
+                    let proposal: Proposal = await contractReader.proposals(i)
                     proposals.push(proposal);
                 }
                 resolve(proposals)
