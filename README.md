09d667f59a9437292a0e59e5d0bad3d2bf63ba35
KipCrossing <kip.crossing@gmail.com>
2020-06-08 12:56:30 +1000

    Add readme

---
+++ README.md
@@ -0,0 +1,27 @@
+# Liquid Decisions
+
+
+### Checkout repo
+
+```
+git clone https://github.com/bazmatic/liquid-decisions
+```
+
+
+### Install the deps
+
+```
+npm install
+```
+
+### Install truffle
+
+```
+npm install truffle --global
+```
+
+### Compile Contract 
+
+```
+truffle compile
+```
--- python/experiment.py
+++ python/experiment.py
@@ -6,16 +6,20 @@ from web3.middleware import construct_sign_and_send_raw_middleware
 
 private_key = os.getenv('PRIVATE_KEY')
 CONTRACT_ADDRESS = '0x95205ad8f8D92967fa35911B9dD7F4e06769eed8'
-
+CONTRACT_ADDRESS = '0x484D17378476e58F6d28806dbd9928b3dF522D62' # Use old contract 
 w3 = Web3(Web3.HTTPProvider("https://kovan.infura.io/v3/663bcd65903948a6b53cd96866fc1a4a"))
 acct = Account.from_key(private_key)
 
+cwd = os.path.dirname(os.path.realpath(__file__)).split('/')
+cwd.pop(-1)
+parent_dir = '/'.join(x for x in cwd)
+print(parent_dir)
 print("Private key: " + acct.privateKey.hex())
 print("Address: " + acct.address)
 print("Balance: " + str(w3.eth.getBalance(acct.address)))
 
 #w3.eth.defaultAccount = acct
-abi = json.load(open("../ethereum/contracts/build/LiquidDecisions.abi"))
+abi = json.load(open(parent_dir + "/ethereum/contracts/build/LiquidDecisions.abi"))
 
 #Kovan
 LiqDec = w3.eth.contract(address=CONTRACT_ADDRESS, abi=abi)
